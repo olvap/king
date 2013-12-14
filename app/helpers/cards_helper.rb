cfedfd49c85e543102d5a064c7f7c0112fb36528
Pablo Oldani <oldani.pablo@gmail.com>
2013-12-14 15:42:21 -0300

    Add helper for numbers.

--- app/helpers/cards_helper.rb
+++ app/helpers/cards_helper.rb
@@ -3,6 +3,16 @@ module CardsHelper
     text = text.gsub('[Energy]', '<i class="fa fa-bolt"></i>').html_safe
     text = text.gsub('[Star]', '<i class="fa fa-star"></i>').html_safe
     text = text.gsub('[Heart]', '<i class="fa fa-heart"></i>').html_safe
-    text = text.gsub('[Attack]', '<i class="fa fa-rocket"></i>').html_safe
+    text = text.gsub('[Attack]', image_tag("attack.png")).html_safe
+    text = text.gsub('[1]', dice_number(1)).html_safe
+    text = text.gsub('[2]', dice_number(2)).html_safe
+    text = text.gsub('[3]', dice_number(3)).html_safe
+  end
+
+  def dice_number number
+    "<span class='fa-stack'>
+        <i class='fa fa-square fa-stack-1x'></i>
+        <i class='fa-stack-1x dice'>#{ number }</i>
+    </span>"
   end
 end
